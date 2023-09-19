70b693ea3e32726fa48072d89fd39eaf150bc72b
dximpress <dixancarballo@gmail.com>
2023-09-19 13:06:12 -0500

    Update codigo.php

--- codigo.php
+++ codigo.php
@@ -1,3 +1,10 @@
 <?php
  echo "Mi primero archivo PHP desde Visual Studio para GitHub";
+
+ function subirPDF(archivo){
+    $archivo = archivo;
+    if($archivo){
+        echo "Archivo subido correctamente";
+    }
+ }
 ?>
