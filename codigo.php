ea0bf8e894410805892c7c0108ec33788e57fde1
dximpress <dixancarballo@gmail.com>
2023-09-19 18:14:24 -0500

    Chrome Devs

--- codigo.php
+++ codigo.php
@@ -4,7 +4,7 @@
  function subirPDF(archivo){
     $archivo = archivo;
     if($archivo){
-        echo "Archivo subido correctamente a la Plataforma desde Visual Studio Code";
+        echo "Archivo subido correctamente a la Plataforma desde Visual Studio Code, ahora con Google Chrome DEVs";
     }
  }
 ?>
