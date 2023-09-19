582a74baef045cfaf2459e477e9def8b01cf8a5f
dximpress <dixancarballo@gmail.com>
2023-09-19 15:46:41 -0500

    Probando desde Visual Studio

--- codigo.php
+++ codigo.php
@@ -4,7 +4,7 @@
  function subirPDF(archivo){
     $archivo = archivo;
     if($archivo){
-        echo "Archivo subido correctamente";
+        echo "Archivo subido correctamente a la platarma";
     }
  }
 ?>
